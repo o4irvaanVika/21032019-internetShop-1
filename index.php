74a3bc8d7659c1ca838c390c09bd8da7f9c3287c
IrKastelceva <kastelceva@gmail.com>
2019-07-18 21:34:53 +0300

    Коммент

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    // Привет всем разработчикам! Это главная страница проекта
+    // Привет всем разработчикам! Это главная страница проекта. Бездельники всё сломают(
     $title = 'главная страница';
     include $_SERVER['DOCUMENT_ROOT'] . '/modules/head.php';
     include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php';
